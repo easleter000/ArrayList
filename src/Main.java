f581cd5849b3c909a25c0ccea7bfebc57eb4ef30
Terry Easley <easleter000@warren.k12.in.us>
2025-02-10 12:59:22 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.add("Chevy");
+        System.out.println(cars);
+        cars.remove(2);
+        System.out.println(cars);
+        Collections.sort(cars);
+        System.out.println(cars);
+
+
+
+    }
 }
